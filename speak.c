919343f67958ae0a1c3216b4b41c1042fa26b8e7
Diomidis Spinellis <dds@aueb.gr>
2021-01-01 15:00:04 +0200

    Remove trailing spaces

--- speak.c
+++ speak.c
@@ -163,7 +163,7 @@ int vs = 2;
 int
 main(argc,argv)
 int argc;
-char **argv; 
+char **argv;
 {
 	char register *t,*u;
 	int i;
@@ -178,35 +178,35 @@ char **argv;
 	if(argc>1 && *argv[1]=='-') {
 loop:	
 		switch(*(argv[1]++)) {
-		default: 
+		default:
 			goto loop;
-		case 'e': 
+		case 'e':
 			eflag = 0;
 			goto loop;
-		case 'p': 
+		case 'p':
 			pflag = 0;
 			goto loop;
-		case 's': 
+		case 's':
 			sflag = 0;
 			goto loop;
-		case 'v': 
+		case 'v':
 			vflag = 0;
 			goto loop;
-		case '\0':; 
+		case '\0':;
 		}
-		argc--; 
-		argv++; 
+		argc--;
+		argv++;
 	}
 	if(vflag) vs = argc>2?creat(argv[2],0666):open("/dev/vs",1);
 	readin(argc>1 ? argv[1]:"/usr/lib/speak.m");
 	for(;;)	{
 
 		t = line;
-		do	{ 
+		do	{
 			if(!read(0,t,1)) {
-				exit(0); 
+				exit(0);
 			}
-		} 
+		}
 		while(*t++!='\n');
 		*(t-1)=' ';
 		*t=0;
@@ -221,12 +221,12 @@ loop:
 			if(*t) readin(t);
 			break;
 		case 'd':
-			if(phread(work,buf+1)!=buf+1) 
+			if(phread(work,buf+1)!=buf+1)
 				decode(1,buf+1);
 			else {
 				tflag = 1;
 				phpron(work,buf);
-				tflag = 0; 
+				tflag = 0;
 			}
 			write(1,"\n",1);
 			break;
@@ -236,7 +236,7 @@ loop:
 			break;
 		case 'l':
 			i = 0;
-			while(++i<ttop) 
+			while(++i<ttop)
 				list(1,&strings[table[i].word]);
 			break;
 		default:
@@ -245,9 +245,9 @@ loop:
 		}
 		else	{
 			if(!replace()&&line[1]!='\0') {
-				t = line; 
-				u = work; 
-				lflag = 0; 
+				t = line;
+				u = work;
+				lflag = 0;
 				for(;;) {
 					while(*t==' '||*t=='\t') t++;
 					while((*u= *t++)!=' '&&*u!='\t') {
@@ -255,14 +255,14 @@ loop:
 							if('a'<=*u&&'z'>=*u
 							    ||*u=='%')
 								lflag++;
-							u++; 
+							u++;
 						}
-						else goto next; 
+						else goto next;
 					}
-					*u++ = 0; 
+					*u++ = 0;
 				}
 next:
-				wtop = u; 
+				wtop = u;
 			}
 			t = work;
 			while(t<wtop){
@@ -291,7 +291,7 @@ next:
 					if((*w=yflag)&&u!=buf+1) {
 						/*pause for punct*/
 						*u++ = 001;
-						*u++ = 001; 
+						*u++ = 001;
 					}
 				}
 noword:
@@ -303,7 +303,7 @@ noword:
 				*u++ = 0174;	/* temp */
 				*u++ = 0;
 				if(vflag) write(vs,buf,u-buf);
-				while(*t++); 
+				while(*t++);
 			}
 		}
 	}
@@ -312,7 +312,7 @@ noword:
 void
 decode(f,s)
 int f;
-char *s; 
+char *s;
 {
 	int b,c;
 	int register flag;
@@ -323,17 +323,17 @@ char *s;
 			b ='3'- ((c&0377)>>6);
 			if(c==001) {
 				flag = 0;
-				c = '%'; 
+				c = '%';
 			}
 			else {
 				if(b!='2') write(1,&b,1);
 				c = c&077;
-				c = dencode(&code[1],&code[0],c); 
+				c = dencode(&code[1],&code[0],c);
 			}
 		}
 		while(c){
 			write(f,&c,1);
-			c>>=8; 
+			c>>=8;
 		}
 	}
 }
@@ -359,7 +359,7 @@ int at1[], at2[], c;
 	struct decenc *t1 = (struct decenc *)at1, *t2 = (struct decenc *)at2;
 	int register i,d;
 	for(i=0;d=t1[i].x;i++) if(c==d) break;
-	return(t2[i].x); 
+	return(t2[i].x);
 }
 
 int
@@ -370,11 +370,11 @@ replace(){
 	t = line;
 	u = buf;
 	if(*t++!=',')return(0);
-	for(;;) { 
+	for(;;) {
 		if(*t=='%') {
 			*u++ = 001;
 			while((*u = *++t) && *u!=' ') u++;
-			break; 
+			break;
 		}
 		b = 1;
 		if(*t<='3') if(*t>='0')
@@ -383,22 +383,22 @@ replace(){
 		while(*t!=',' && *t!=' ' && *t!=0) {
 			i = *t++;
 			if(n) i<<=8;
-			n |= i; 
+			n |= i;
 		}
 		n = dencode(&code[0],&code[1],n);
 		if(n) *u++ = n+ (b<<6);
 		if(*t!=',' && *t!=' ') break;
-		t++; 
+		t++;
 	}
 	*u++=0;
 	phwrite(work,buf);
-	return(1); 
+	return(1);
 }
 
 void
 list(f,s)
 int f;
-char *s; 
+char *s;
 {
 	char register *t;
 	if(phread(s,buf)==buf) return;
@@ -406,15 +406,15 @@ char *s;
 	t = s;
 	while(*t) write(f,t++,1);
 	write(f,"\n",1);
-	decode(f,buf); 
-	write(f,"\n",1); 
+	decode(f,buf);
+	write(f,"\n",1);
 }
 
 void
 copy(){
 	char buf1[100];
 	phread(work,buf1);
-	phwrite(name(),buf1); 
+	phwrite(name(),buf1);
 }
 
 char *
@@ -425,7 +425,7 @@ name(){
 	t = buf;
 	while(*u && (*t = *u++)!=' ') t++;
 	*t = 0;
-	return(buf); 
+	return(buf);
 }
 
 void
@@ -435,19 +435,19 @@ char *file;
 	int register f;
 	if((f = open(file,0))<0) {
 		diag(2);
-		return; 
+		return;
 	}
 	read(f,&ttop,2);
 	read(f,table,recsize*ttop);
 	read(f,&stop,2);
 	read(f,strings,stop);
-	close(f); 
+	close(f);
 }
 
 int
 writo1(f,u,n)
 int f, n;
-int *u; 
+int *u;
 {
 	int register i,j,k;
 	i = j = *u;
@@ -455,7 +455,7 @@ int *u;
 	k = 1;
 	while(strings[i++]) k++;
 	write(f,&strings[j],k);
-	return(k); 
+	return(k);
 }
 
 void
@@ -466,26 +466,26 @@ char *file;
 	int n;
 	if((f=creat(file,0666))<0) {
 		diag(3);
-		return; 
+		return;
 	}
 	lseek(f,recsize*ttop+4,0); /*get to byte 0 of string store*/
 	write(f,strings,1);	/*and put it out*/
 	n = 1;
 	for(i=1;i<ttop;i++) {
 		n += writo1(f,&table[i].word,n);
-		n += writo1(f,&table[i].phon,n); 
+		n += writo1(f,&table[i].phon,n);
 	}
 	lseek(f,0,0);
 	write(f,&ttop,2);
 	write(f,table,recsize*ttop);
 	write(f,&n,2);	/*new value of stop */
-	close(f); 
+	close(f);
 }
 
 
 int
 find(in)
-char *in; 
+char *in;
 {
 	int register bot,top,i;
 	int z;
@@ -496,14 +496,14 @@ char *in;
 		z = compare(in,&strings[table[i].word]);
 		if(z==0) break;
 		if(z<0) top = i;
-		else bot = i; 
+		else bot = i;
 	}
-	return(i); 
+	return(i);
 }
 
 int
 prefix(in)
-char *in; 
+char *in;
 {
 	char register *u,*s;
 	char *end;
@@ -522,50 +522,50 @@ loop:
 					if(*u==0) return(i);
 					if(*s!=0) continue;
 					pref = bot = i;
-					end = u+1; 
+					end = u+1;
 				}
 				else if(u<=end) bot = i;
 				else break;
-				goto loop; 
+				goto loop;
 			}
-		} 
+		}
 		while((i=(bot+i)/2)>bot);
 		bot++;
-		end++; 
+		end++;
 	}
-	return(pref); 
+	return(pref);
 }
 
 int
 compare(a,b)
-char *a,*b; 
+char *a,*b;
 {
 	while(*a == *b) {
 		if(*a==0) return(0);
 		a++;
-		b++; 
+		b++;
 	}
-	return(*a<*b ? -1 : 1); 
+	return(*a<*b ? -1 : 1);
 }
 
 char *
 phread(in,out)	/* returns address of letter after output string */
-char *in,*out; 
+char *in,*out;
 {
 	char *s;
 	int i;
 	i = find(in);
 	if(compare(in,&strings[table[i].word])==0) {
 		for(s = &strings[table[i].phon];*out = *s++;)
-			out++; 
+			out++;
 	}
-	*out = 0; 
-	return(out); 
+	*out = 0;
+	return(out);
 }
 
 void
 phwrite(in,out)
-char *in, *out; 
+char *in, *out;
 {
 	int register i,j,z;
 	i = find(in);
@@ -574,36 +574,36 @@ char *in, *out;
 		i++;
 		if(ttop>=NT) {
 			diag(4);
-			return; 
+			return;
 		}
 		for(j=ttop;j>i;j--) {
 			table[j].word = table[j-1].word;
-			table[j].phon = table[j-1].phon; 
+			table[j].phon = table[j-1].phon;
 		}
 		table[i].word = stop;
 		stash(in);
-		ttop++; 
+		ttop++;
 	}
 	else if(*out==0) {
 		for(j=i;j<ttop;j++) {
 			table[j].word = table[j+1].word;
-			table[j].phon = table[j+1].phon; 
+			table[j].phon = table[j+1].phon;
 		}
 		ttop--;
-		return; 
+		return;
 	}
 	table[i].phon = stop;
-	stash(out); 
+	stash(out);
 }
 
 void
 stash(s)
-char *s; 
+char *s;
 {
 	while(stop<NS)
 		if((strings[stop++]= *s++)==0)
-			return; 
-	diag(5); 
+			return;
+	diag(5);
 }
 
 void
@@ -612,13 +612,13 @@ int n;
 {
 	char register *p;
 	p = diags[n];
-	while(*p) write(1,p++,1); 
-	write(1,"\n",1); 
+	while(*p) write(1,p++,1);
+	write(1,"\n",1);
 }
 
 char *
 phspell(in,out)
-char *in, *out; 
+char *in, *out;
 {
 	char register *c,*t;
 	c = "* \0";
@@ -630,17 +630,17 @@ char *in, *out;
 			*out++ = 0346;	/* ,0k */
 			*out++ = 0367;	/* ,0a2 */
 		}
-		if(*in) *out++ = 0101; 
+		if(*in) *out++ = 0101;
 	}	/*phoneme 2-0*/
 	*out = 0;
-	return(out); 
+	return(out);
 }
 
 
 /*danger--reuses "line" to conserve space*/
 char *
 phpron(in,out)
-char *in, *out; 
+char *in, *out;
 {
 	char register *t,*u;
 	char *s;
@@ -649,7 +649,7 @@ char *in, *out;
 	int i;
 	sout = out;
 	*sout = 0;
-	s = t = line+2; 
+	s = t = line+2;
 	u = in;
 	while(*s++ = *u++);
 	s -= 2;
@@ -658,36 +658,36 @@ char *in, *out;
 		return(out);
 	if(s==t||vowel(t,s+1)<t) {
 		/*spell one-letter words and vowelless words*/
-		goto done; 
+		goto done;
 	}
 	if(eflag) {	/* handle english endings*/
-		if(sflag = finals(t,&s)) 
+		if(sflag = finals(t,&s))
 			if(sout!=(out=phread(t,out))) {
 				*out++ = sflag>1?0140:0155;
 				*out++ = 0140; /*,s,s or ,z,s*/
-				goto done; 
+				goto done;
 			}
 		midu(t,s);
 		finale(t,&s);
 		mide(t,&s);
 		mids(t,s);
-		if(sflag) *++s = 's'; 
+		if(sflag) *++s = 's';
 	}
-	*--t = '#'; 
-	*++s = '#'; 
+	*--t = '#';
+	*++s = '#';
 	*++s = 0;
 	while(*t) {
 		*--t = '%';
 		i = prefix(t);
 		if(i==0) {
-			*sout = 0; 
-			return(sout); 
+			*sout = 0;
+			return(sout);
 		}
 		u = &strings[table[i].word];
 		while(*u) {
 			t++;
 			if(tflag) write(1,u,1);
-			u++; 
+			u++;
 		}
 		if(tflag) write(1," ",1);
 		s = &strings[table[i].phon];
@@ -696,18 +696,18 @@ char *in, *out;
 			else	{	/*do replacement*/
 				u = s;
 				while(*u) u++;
-				while(--u>=s) *--t = *u; 
-				break; 
+				while(--u>=s) *--t = *u;
+				break;
 			}
 	}
 done:
 	*out = 0;
-	return(out); 
+	return(out);
 }
 
 char
 finals(in,ls)
-char *in,**ls; 
+char *in,**ls;
 {
 	char register *end;
 	int val;
@@ -716,25 +716,25 @@ char *in,**ls;
 	if(*end=='s'&&!oneof(end[-1],"us")) {
 		*end-- = 0;
 		if(*end=='\'') *end-- = 0;
-		val = oneof(*end,"cfkpt")+1; 
+		val = oneof(*end,"cfkpt")+1;
 	}
 	if(*end=='e'&&end[-1]=='i') {
 		*end-- = 0;
-		*end = 'y'; 
+		*end = 'y';
 	}
 	*ls = end;
-	return(val); 
+	return(val);
 }
 
 void
 midu(in,end)
 char *in,*end;
 {
-	char register *s,*t; 
+	char register *s,*t;
 	for(s=in;s<end-1;s++) if(*(t=s)=='u' && !oneof(s[-1],aeiou)) {
 		if(oneof(s[1],aeiouwxy)) continue;
 		if(s[2]=='r'&&oneof(s[1],bcdfgkpt)) s++;
-		if(oneof(s[2]|040,aeiouy)) *t = 'U'; 
+		if(oneof(s[2]|040,aeiouy)) *t = 'U';
 	}
 	for(s=in;s<end-2;s++) if(oneof(*(t=s),aeo)) {
 		if(oneof(s[1],"aeiouwxy|"))continue;
@@ -742,12 +742,12 @@ char *in,*end;
 		if(s[2]=='r'&&s[3]=='i'&&oneof(s[1],bcdfgkpt)) s++;
 		if(oneof(s[2],"ie")&&oneof(s[3]|040,aou)
 		    || s[2]=='i'&&s[3]=='e'&&s[4]=='n')
-			*t ^= 040; 
+			*t ^= 040;
 	}
 	s = in;
 	if(*s=='y') s++;
 	while(!oneof(*s|040,aeiouy)&&s<end) s++;
-	if(oneof(*s,"iy") && oneof(s[1],aou)) *s ^= 040; 
+	if(oneof(*s,"iy") && oneof(s[1],aou)) *s ^= 040;
 }
 
 char *suff0[] = {
@@ -784,7 +784,7 @@ char *suff3[] = {
 
 void
 finale(in,ls)
-char *in,**ls; 
+char *in,**ls;
 {
 	char register *t,*end,*u;
 	char *s;
@@ -792,7 +792,7 @@ char *in,**ls;
 	end = *ls;
 	if((*end=='e')&&vowel(in,end)<in) {
 		*end = 'E';	/* monosyllable in -e */
-		return; 
+		return;
 	}
 	t = suffix(in,end,suff0);
 	if(t<end) t = longe(in,t);
@@ -800,36 +800,36 @@ char *in,**ls;
 		t = end;
 		while((u=suffix(in,t,suff1))!=t) {
 			insert(u+1,ls);
-			t = u; 
+			t = u;
 		}
 		if((u=suffix(in,t,suff2))!=t) {
 			insert(u+1,ls);
-			return; 
+			return;
 		}
 		if((u=suffix(in,t,suff3))!=t) {
 			if(u[2]=='e') return;
 			insert(u+1,ls);
-			t = u; 
+			t = u;
 		}
 		if(oneof(*t,"iuy")&&vowel(in,t)<in) {
 			*t ^=040;	/*monosyllables in -y, */
-			return; 
+			return;
 		}	/*perhaps suffixed*/
 		if(!oneof(t[t[1]=='|'?2:1],"aeio")) return;
 		t = longe(in,t);
 		if(t<in || oneof(t[1],"cg")&&vowel(in,t)>=in) return;
 		if(th(t+1)) {
-			t[1] = 'T'; 
-			t[2] = 'H'; 
-		} 
+			t[1] = 'T';
+			t[2] = 'H';
+		}
 	}
 	if((t==in||!oneof(t[-1],aeo)) && !(*t=='e'&&t[1]=='l'))
-		*t ^= 040; 
+		*t ^= 040;
 }
 
 void
 mide(in,ls)
-char *in,**ls; 
+char *in,**ls;
 {
 	char register *u,*end;
 	end = *ls;
@@ -845,52 +845,52 @@ char *in,**ls;
 			    &&!oneof(u[-1],"aehiouwxy")
 			    &&oneof(u[-2],"aiouyU")
 			    &&!oneof(u[-3],"aeiu")) {
-				if(u[-3]!='o') u[-2] &= ~040; 
+				if(u[-3]!='o') u[-2] &= ~040;
 			}
 			else return;
 shift:		
-			insert(u+1,ls); 
-		} 
+			insert(u+1,ls);
+		}
 }
 
 void
 mids(in,end)
-char *in,*end; 
+char *in,*end;
 {
-	while(++in<end) 
+	while(++in<end)
 		if(*in=='s'&&oneof(in[-1]|040,"aeiouy")
 		    &&oneof(in[1]|040,"aeimouy"))
-			*in ^= 040; 
+			*in ^= 040;
 }
 
 int
 syltest(in,s,end)
-char *in,*end,*s; 
+char *in,*end,*s;
 {
 	if(!oneof(*in|040,s))
 		while(++in<end) {
 			if(*in=='e'&&in[1]=='|') break;
 			if(*in=='|') break;
-			if(oneof(*in|040,aeiouy)) return(1); 
+			if(oneof(*in|040,aeiouy)) return(1);
 		}
-	return(0); 
+	return(0);
 }
 
 void
 insert(in,ls)
-char *in,**ls; 
+char *in,**ls;
 {
 	char register *s,*end;
 	end = *ls;
 	if(*in=='e') if(*++in=='|') return;
 	for(s=++end;s>=in;s--) s[1] = *s;
-	*in = '|'; 
-	*ls = end; 
+	*in = '|';
+	*ls = end;
 }
 
 char *
 suffix(in,end,s)
-char *in,*end,**s; 
+char *in,*end,**s;
 {
 	char register *t,*u;
 	while(u = *s) {
@@ -898,36 +898,36 @@ char *in,*end,**s;
 		while(*u == *--t) u++;
 		if(*u==0) {
 			if(vowel(in,t+1)<in) break;
-			else return(t); 
+			else return(t);
 		}
-		s++; 
+		s++;
 	}
-	return(end); 
+	return(end);
 }
 
 char *
 longe(in,end)
-char *in, *end; 
+char *in, *end;
 {
 	if(th(end-1)) end--;
-	return(!oneof(*end|040,aeiouwxy)&&oneof(*--end,aeiouy)?end:in-1); 
+	return(!oneof(*end|040,aeiouwxy)&&oneof(*--end,aeiouy)?end:in-1);
 }
 
 int
 oneof(c,l)
 char c,*l;
-{ 
+{
 	while(*l) if(c == *l++) return(1);
-	return(0); 
+	return(0);
 }
 
 char *
 vowel(in,end)
-char *in,*end; 
+char *in,*end;
 {
 	while(--end>=in)
 		if(oneof(*end|040,aeiouy)) break;
-	return(end); 
+	return(end);
 }
 
 int
@@ -936,14 +936,14 @@ char *s;
 {
 	if('A'<=*s && *s <='Z') {
 		*s ^= 040;
-		return(1); 
+		return(1);
 	}
-	return(0); 
+	return(0);
 }
 
 int
 th(s)
-char *s; 
+char *s;
 {
-	return(*s=='t'&s[1]=='h'); 
+	return(*s=='t'&s[1]=='h');
 }
